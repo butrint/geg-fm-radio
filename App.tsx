730444f2dbf9298c9e7f96d4ad38095143da7f33
Butrint Rashiti <rashiti.butrint@gmail.com>
2024-10-22 15:44:41 +0200

    Added all what we need

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Dimensions, View, Image } from 'react-native';
+import AudioPlayer from './src/components/AudioPlayer';
 
-export default function App() {
+const { width, height } = Dimensions.get('window');
+
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('./assets/geg-fm-purple-lime.png')} // Your image path
+          style={styles.image}
+          resizeMode="contain" // Ensures the image is scaled to fit while maintaining aspect ratio
+        />
+      </View>
+      <AudioPlayer />
+    </SafeAreaView>
   );
 }
+
+console.log("das")
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'rgb(14,17,36)'
+  },
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'center', // Centers the image vertically
+    alignItems: 'center', // Centers the image horizontally
+  },
+  image: {
+    width: width * 0.9, // Image width is 90% of the screen width
+    height: height * 0.5, // Image height is 50% of the screen height
+    maxWidth: 400, // Set a maximum width to prevent the image from getting too large on bigger screens
+    maxHeight: 400, // Set a maximum height
   },
 });
---
+++ src/components/AudioPlayer.tsx
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react';
+import { View, Button, Text, StyleSheet, ImageBackground, TouchableOpacity, Pressable } from 'react-native';
+import { Audio, InterruptionModeAndroid, InterruptionModeIOS } from 'expo-av';
+import { Ionicons } from '@expo/vector-icons';
+import { Sound } from 'expo-av/build/Audio';
+
+const AudioPlayer = () => {
+    const [sound, setSound] = useState<Sound | null>(null);
+    const [isPlaying, setIsPlaying] = useState(false)
+
+    useEffect(() => {
+        return () => {
+            // Unload sound when the component unmounts
+            if (sound) {
+                sound.unloadAsync();
+            }
+        };
+    }, [sound]);
+
+    const playPauseSound = async () => {
+        try {
+            await Audio.requestPermissionsAsync();
+            await Audio.setAudioModeAsync({
+                staysActiveInBackground: true,
+                interruptionModeAndroid: InterruptionModeAndroid.DoNotMix,
+                shouldDuckAndroid: false,
+                playThroughEarpieceAndroid: false,
+                allowsRecordingIOS: false,
+                interruptionModeIOS: InterruptionModeIOS.DoNotMix,
+                playsInSilentModeIOS: true,
+            });
+            if (sound === null) {
+                // Load and play the streaming audio from a URL
+                const { sound: newSound } = await Audio.Sound.createAsync(
+                    { uri: 'https://a7.asurahosting.com:6980/radio.mp3' },
+                    { shouldPlay: true } // Replace with your streaming URL
+                );
+                setSound(newSound);
+                await newSound.playAsync();
+                setIsPlaying(true);
+            } else {
+                if (isPlaying) {
+                    await sound.pauseAsync();
+                    setIsPlaying(false);
+                } else {
+                    await sound.playAsync();
+                    setIsPlaying(true);
+                }
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    return (
+        <Pressable style={styles.fullScreenPressable} onPress={playPauseSound}>
+            <ImageBackground source={require('../../assets/geg-fm-mic.png')} style={styles.background} imageStyle={styles.image}>
+                <View style={styles.container}>
+                    <TouchableOpacity style={styles.button} onPress={playPauseSound} >
+                        <Ionicons
+                            name={isPlaying ? 'pause' : 'play'}
+                            size={32}
+                            color="#fff" // Icon color
+                        />
+                    </TouchableOpacity>
+                </View>
+            </ImageBackground>
+        </Pressable>
+    );
+};
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        resizeMode: 'cover', // or 'contain' depending on your image
+        width: '100%', // Ensure the background covers the entire width
+        height: '100%', // Ensure the background covers the entire height
+        justifyContent: 'center', // Center the child content (AudioPlayer)
+        alignItems: 'center',
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        backgroundColor: 'transparent',
+        marginBottom: 20
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 20,
+    },
+    image: {
+        resizeMode: 'cover', // 'cover' ensures the image covers the area entirely
+    },
+    button: {
+        width: 70, // Width of the button
+        height: 70, // Height of the button
+        paddingTop: 20,
+        backgroundColor: 'transparent', // Transparent background
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderColor: 'transparent',
+    },
+    fullScreenPressable: {
+        flex: 1,
+    },
+});
+
+export default AudioPlayer;
